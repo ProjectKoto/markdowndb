945d32a65c5b4242c0393e369c7d3b29e9403d4f
shunf4 <shun1048576@gmail.com>
2026-08-04 02:50:40 +0800

    tk: remove asset_raw_path dup check in MddbFile.batchDelIfExistThenInsert

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -130,9 +130,11 @@ class MddbFile {
     if (!areUniqueObjectsByKey(files, "_id")) {
       throw new Error("Files must have unique _id");
     }
-    if (!areUniqueObjectsByKey(files, "asset_raw_path")) {
-      throw new Error("Files must have unique asset_raw_path");
-    }
+
+    // necessary?
+    // if (!areUniqueObjectsByKey(files, "asset_raw_path")) {
+    //   throw new Error("Files must have unique asset_raw_path");
+    // }
 
     const serializedFiles = files.map((file) => {
       const serializedFile: any = {};
